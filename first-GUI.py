d100622ad3a3f8f50f60266e05655fba41c4ca57
DianaMayesCloud <diana.lmayes@gmail.com>
2021-03-06 15:02:35 -0500

    updated with comments

--- first-GUI.py
+++ first-GUI.py
@@ -7,17 +7,23 @@ picture = [
   [0,0,0,1,0,0,0]
 ]
 
-for rowindex, array in enumerate(picture):
-  array = picture[rowindex]
-  for index, value in enumerate(array):
-    if value == 0:
+
+# My Code
+
+for rowindex, array in enumerate(picture):          # loop through each row
+  array = picture[rowindex]                         # create a new list variable
+  for index, value in enumerate(array):             # loop through each item in a row 
+    if value == 0:                                  # if the value is 0, change the value to a space
       array[index] = ' '
     else:
-      array[index] = '*'
-  print(''.join(array))
+      array[index] = '*'                            # else, change the value to a star 
+  print(''.join(array))                             # join the values, eliminating brackets and commas
 
 
 #Andrei's Code:
+# Note: better code because 
+# 1) it does not create an additional variable 
+# 2) it does not modify the original variable 
 
 # for row in picture:
 #   for pixel in row: 
